99594f7d7c3dc66017e49fbf44a3dfbd081c01bc
Sarah <sarah_suttiratana@hotmail.com>
2025-03-20 15:12:47 -0700

    Added Email Template HTML Markup and Added Sample Data to Send_Email Script
    - Added sample data to sending email script
    - Added HTML markup of email template

--- send_email.py
+++ send_email.py
@@ -7,9 +7,18 @@ from sendgrid.helpers.mail import Mail
 message = Mail(
     from_email='sarahsuttiratana@gmail.com',
     to_emails='sarahsuttiratana@gmail.com',
-    subject='Sending with Twilio SendGrid is Fun',
-    html_content='<strong>and easy to do anywhere, even with Python</strong>')
+    subject='Price Drop Alert',
+    html_content='<strong>Sample html content</strong>')
 try:
+    # Set the dynamic template ID
+    message.dynamic_template_data = {
+        'Product_Name': 'Ribbed Cropped Bra Top',
+        'Product_URL': 'https://www.uniqlo.com/us/en/products/E473983-000/00?colorDisplayCode=02&sizeDisplayCode=003',
+        'Original_Price': '29.90',
+        'Sale_Price': '14.90',
+        'Discount_Amount': '15.00'
+    }
+    message.template_id = 'd-9058e6d5e3cb48609f015324fccffe27'
     sg = SendGridAPIClient(os.environ.get("SENDGRID_API_KEY"))
     response = sg.send(message)
     print(response.status_code)
